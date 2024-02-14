989326866b2f931d8200fe9d1a82c0aac178dad3
Mehari-Temesgen <meharics2011@gmail.com>
2024-02-14 10:27:47 +0300

    server route update

--- routes/AnswerRoute.js
+++ routes/AnswerRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authmiddleware = require("../middleware/authmiddleware");
 const { postanswer, getAnswer } = require("../controller/AnswerControl");
-
-router.post("/:questionid/create", authmiddleware, postanswer);
 router.get("/:questionid", authmiddleware, getAnswer);
+router.post("/:questionid/create", authmiddleware, postanswer);
 module.exports = router;
--- routes/QuestionRoute.js
+++ routes/QuestionRoute.js
@@ -2,9 +2,6 @@ const express = require("express");
 const authmiddleware = require("../middleware/authmiddleware");
 const { postquestion, getquestions } = require("../controller/QuestionControl");
 const router = express.Router();
-router.get("/all-question", authmiddleware, (req, res) => {
-  res.send("all question");
-});
-router.post("/onequestion", postquestion);
-router.get("/allquestion", getquestions);
+router.get("/", authmiddleware, getquestions);
+router.post("/create", authmiddleware, postquestion);
 module.exports = router;
